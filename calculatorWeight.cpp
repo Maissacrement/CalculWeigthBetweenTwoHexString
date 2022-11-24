e827476614ef548cee4308a214c0e9eaee9c5284
delar <emmariodelar@gmail.com>
2022-11-24 03:18:52 +0100

    Calc error

--- calculatorWeight.cpp
+++ calculatorWeight.cpp
@@ -5,6 +5,34 @@
 using namespace std;
 namespace py = pybind11;
 
+string base[] = { "746370", "207564" };
+
+string entry(string chaine) {
+    string matchingID(chaine);
+    string hex, mt;
+    long weight=0;
+    long resError=-1;
+    string resl="";
+    for (size_t j = 0; j < size(base); j++) {
+        for (size_t i = 0; i < matchingID.length() / 2; i++)
+        {
+            hex=base[j][i*2];
+            hex+=base[j][i*2+1];
+            mt=matchingID[i*2];
+            mt+=matchingID[i*2+1];
+            
+            weight+=abs(strtol(hex.c_str (), NULL, 16) - strtol(mt.c_str (), NULL, 16) + 0);
+        }
+        if (resError == -1 || resError > weight) {
+            resError=weight;
+            resl=base[j];
+        }
+        weight=0;
+    }
+    
+    return "[" + std::to_string(resError) + ",'" + resl + "']";
+}
+
 string getMatchingCase(string id, string dict)
 {
     string matchingID(id); // "7f26374e1e5542b38fb825634c3daf47"
@@ -48,5 +76,6 @@ int main() {
 PYBIND11_MODULE(calcWeight, m)
 {
     m.def("getMatchingCase", &getMatchingCase);
+    m.def("getEntry", &entry);
 }
 
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from build.calcWeight import getMatchingCase
+from build.calcWeight import getMatchingCase, getEntry
 
 print(
-    getMatchingCase('7f26374e1e5542b38fb82563411c6854b0'[:32], "./guid3")
+    getMatchingCase('7f26374e1e5542b38fb82563411c6854b0'[:32], "./ids/guid.csv")
+)
+
+print(
+    getEntry('7f26374e1e5542b38fb82563411c6854b0'[:6])
 )
